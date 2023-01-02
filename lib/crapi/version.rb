b48ee65b745822804de8058808c7737f38844234
Nestor Custodio <sakimorix@gmail.com>
2023-01-02 15:25:52 -0600

    version: release v1.0.0

--- lib/crapi/version.rb
+++ lib/crapi/version.rb
@@ -7,5 +7,5 @@ module CrAPI
   # This should only ever be updated *immediately* before a release; the commit that updates this
   # value should be pushed **by** the `rake release` process.
   #
-  VERSION = '0.1.3'.freeze
+  VERSION = '1.0.0'.freeze
 end
